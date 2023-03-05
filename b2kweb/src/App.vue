0650b5f0ef78dcfe552b65fba7299b22f0027bb8
Guilheim chataing <gchatain@bitume2000.fr>
2023-03-05 14:29:39 +0000

    ajout de la nav

--- b2kweb/src/App.vue
+++ b2kweb/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Home/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/NavBar.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
---
+++ b2kweb/start.sh
@@ -0,0 +1,1 @@
+screen -d -m -S b2kweb npm run serve
