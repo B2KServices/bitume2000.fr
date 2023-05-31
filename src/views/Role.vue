2a182df9540202dbe8595862a31d3f0c3b34bc38
gchatain <gchatain@student.42lyon.fr>
2023-06-01 01:07:16 +0200

    ajout relation page role

--- src/views/Role.vue
+++ src/views/Role.vue
@@ -5,8 +5,8 @@
       <h2>{{ category.name }}</h2>
       <div v-for="(role, roleIndex) in category.children" :key="roleIndex" class="role-item">
         <span>{{ role.name }}</span>
-        <button :class="{ active: role.active }" @click="roleUpdate">
-          {{ role.active ? 'Actif' : 'Inactif' }}
+        <button @click="roleUpdate(role.id)">
+          {{ values[role.id] ? 'Supprimer' : 'Ajouter'}}
         </button>
       </div>
     </div>
@@ -14,21 +14,87 @@
 </template>
 
 <script>
-import axios from 'axios';
-import Cookies from 'js-cookie';
+import axios from "axios";
+import Cookies from "js-cookie";
 
 export default {
   name: 'RolePage',
   data() {
     return {
       categories: [],
-      isLoading: false
+      ownrole: [],
+      isLoading: false,
+      values: {}
     };
   },
   mounted() {
     this.fetchCategories();
+    this.fetchUserRole();
   },
   methods: {
+    completeValues() {
+      for (const category of this.categories) {
+        for (const role of category.children) {
+          this.values[role.id] = this.ownrole.some(r => r.id === role.id); // Vérifier si l'ID du rôle est présent dans this.ownrole
+        }
+      }
+    },
+    roleDelete(id)
+    {
+      axios.delete('https://api.bitume2000.fr/v2/members/role', {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${Cookies.get('token')}`
+        }, data: {
+          role_id: id,
+        }
+      })
+          .then(response => {
+            console.log(response);
+          })
+          .catch(error => {
+            console.log(error);
+          });
+    },
+    roleAdd(id)
+    {
+      axios.post('https://api.bitume2000.fr/v2/members/role', {
+        role_id: id,
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${Cookies.get('token')}`
+        }
+      })
+          .then(response => {
+            console.log(response);
+          })
+          .catch(error => {
+            console.log(error);
+          });
+    },
+    roleUpdate(id)
+    {
+      if (this.values[id])
+        this.roleDelete(id);
+      else
+        this.roleAdd(id);
+      this.values[id] = !this.values[id];
+    },
+    async fetchUserRole() {
+      const response = await axios.get('https://api.bitume2000.fr/v2/members/role', {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${Cookies.get('token')}`
+        }
+      })
+      if (response.data) {
+        this.ownrole = response.data;
+        this.completeValues();
+      } else {
+        this.redirectToAuth();
+      }
+    },
     async fetchCategories() {
       this.isLoading = true;
       try {
@@ -57,8 +123,10 @@ export default {
         this.$router.push('/');
       }
     }
-  }
+  },
 }
+
+
 </script>
 
 <style>
