dee23152fe7f6367f960e0033c6f37de2177adda
gchatain <gchatain@student.42lyon.fr>
2023-06-01 01:32:11 +0200

    check qui sert à rien

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
     devServer: {
-        disableHostCheck: true
+        allowedHosts: "all",
     }
 })
