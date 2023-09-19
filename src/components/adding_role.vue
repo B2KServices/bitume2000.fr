625c9a9ba8b09350d6488c17c5a16c6e92497a10
Guilheim Chataing <guilheimchataing@gmail.com>
2023-09-19 16:20:59 +0200

    en fait si

--- src/components/adding_role.vue
+++ src/components/adding_role.vue
@@ -78,7 +78,7 @@ async function admitRole() {
 		);
 		feedback.value = "demande envoyée avec succès"
 	} catch (e) {
-    feedback.value = response.data.message_code
+    feedback.value = response
 		console.log(e);
 	}
   dispfeedback.value = true;
